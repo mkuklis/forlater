10790b01072daeb69b90f344230f3a5450d8272d
michal <michal@ubi.(none)>
2009-11-18 00:32:43 -0500

    first commit

---
+++ bookmarks.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'redis'
+
+class Bookmarks
+  
+  def initialize
+    @db = Redis.new({:host => '98.129.238.130', :port => 6379})
+  end
+  
+  def get_all
+   @db.list_range 'bookmarks', 0, -1
+  end
+  
+  def add(value)
+    @db.push_head 'bookmarks', value
+    puts      
+  end
+  
+  def remove(index)
+    @db.list_trim 'bookmarks', index-1, index
+  end
+
+end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require 'readlater'
+run Sinatra::Application
